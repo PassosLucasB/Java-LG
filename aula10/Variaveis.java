bd0e5ade9d32e5d86a3acd96275e9330ba96e815
PassosLucasB <PassosL@outlook.com>
2024-04-16 23:51:39 -0300

    add variaveis

--- aula10/Variaveis.java
+++ aula10/Variaveis.java
@@ -3,6 +3,14 @@ public class Variaveis {
 
     public static void main(String[] args) {
 
+        int idade;
+        String nome;
+        String nomeDoMeuCachorro;
+
+        int _idade;
+        int $idade;
+        String ano2014;
+        String nome_do_meu_cachorro;
     }
 
 }
