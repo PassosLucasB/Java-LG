d52424115f64d239392f59a064916c15beedde89
PassosLucasB <PassosL@outlook.com>
2024-04-17 23:48:59 -0300

    final variaveis

--- aula10/Variaveis.java
+++ aula10/Variaveis.java
@@ -3,18 +3,24 @@ public class Variaveis {
 
     public static void main(String[] args) {
 
+        // convenção Java
         int idade = 20;
         String nome = "Loiana";
         String nomeDoMeuCachorro = "totó";
+        String ano2014;
 
+        // aceito, mas não utilizado
         int _idade;
         int $idade;
-        String ano2014;
+
+        // não é convenção JAV
         String nome_do_meu_cachorro;
+        String NomeDoMeuCachorro;
 
         idade = 25;
 
         System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
     }
 
 }
