1e6d89ebebaa7fa1e08e9154497abcca108bc40d
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-04-22 21:54:40 -0300

    Add files via upload
    add aula13

---
+++ aula13/OperadoresAritmeticos.java
@@ -0,0 +1,14 @@
+package aula13;
+
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+
+        int resultado = 1 + 2;
+        System.out.println(resultado);
+
+        resultado = resultado - 1;
+
+    }
+
+}
