ee14e7206b2fad2d0ca45274d23eb80b309ab71d
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-04-25 23:05:45 -0300

    Add files via upload
    add OperadoresLogicos

--- aula13/OperadoresRelacionais.java
+++ aula13/OperadoresRelacionais.java
@@ -7,6 +7,6 @@ public class OperadoresRelacionais {
         int valor1 = 1;
         int valor2 = 2;
 
-        System.out.println("valor1 ==valor2: " + (valor1 == valor2));
+        System.out.println("valor1 == valor2: " + (valor1 == valor2));
     }
 }
