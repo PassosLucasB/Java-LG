c6ce125713505f1302697ad390757b734faceb05
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-04-26 23:28:56 -0300

    Add files via upload

--- aula13/CurtoCircuito.java
+++ aula13/CurtoCircuito.java
@@ -8,6 +8,9 @@ public class CurtoCircuito {
         boolean resultado2 = verdadeiro && falso;
         System.out.println(resultado1);
         System.out.println(resultado2);
+
+        int resultado = 1 + 1 - 1 + 1 * 1 / 1;
+        System.out.println(resultado);
     }
 
 }
