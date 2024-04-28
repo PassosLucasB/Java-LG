62a5f7efac4a98a0fd1cc2db0a746fa3ea491035
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-04-27 23:26:03 -0300

    Add files via upload

--- aula13/OperadoresAritmeticos.java
+++ aula13/OperadoresAritmeticos.java
@@ -27,6 +27,31 @@ public class OperadoresAritmeticos {
         String terceiroNome = primeiroNome + segundoNome;
         System.out.println(terceiroNome);
 
+        resultado = resultado + 1;
+        System.out.println(resultado);
+
+        resultado++;
+        System.out.println(resultado);
+
+        // 5
+
+        System.out.println(resultado++);
+        // mesma coisa que
+        // resultado = resultado + 1;
+        // resultado += 1;
+        // System.out.println(resultado);
+
+        System.out.println(++resultado);
+        // mesma coisa que
+        // resultado += 1;
+        // System.out.println(resultado);
+
+        resultado--;
+        System.out.println(resultado);
+        // mesma coisa que
+        // System.out.println(resultado--);
+        // System.out.println(--resultado);
+
     }
 
 }
