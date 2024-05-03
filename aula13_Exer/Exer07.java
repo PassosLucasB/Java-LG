a7f2c08604c1e61499c911d28cd21a61df3daaa5
PassosLucasB <PassosL@outlook.com>
2024-05-02 23:19:22 -0300

    add exer07

---
+++ aula13_Exer/Exer07.java
@@ -0,0 +1,10 @@
+
+public class Exer07 {
+
+    // Exercicio 07 = Faça um programa que calcule a área de um quadrado
+    // em seguida mostre o dobro desta área para o usuário
+
+    public static void main(String[] args) {
+
+    }
+}
