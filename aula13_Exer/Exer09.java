1305fc38133a58b054c42b74945ba60fcc2c1e15
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-05-04 17:27:56 -0300

    Add files via upload

---
+++ aula13_Exer/Exer09.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Exer09 {
+
+    // Exercicio 09 = Faça um Programa que peça a temperatura em graus Farenheit
+    // transforme e mostre a temperatura em graus Celsius
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+    }
+
+}
