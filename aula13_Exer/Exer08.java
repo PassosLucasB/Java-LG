3c71185acd3acd21dd65a86c1b8538222cc72460
PassosLucasB <PassosL@outlook.com>
2024-05-03 23:16:07 -0300

    add exer06

---
+++ aula13_Exer/Exer08.java
@@ -0,0 +1,12 @@
+
+public class Exer08 {
+
+    // Exercicio 08 = Faça um programa que pergunte quanto você ganha por hora
+    // e o número de horas trabalhadas no mês.
+    // Calcule e mostre o total do seu salário no referido mês.
+
+    public static void main(String[] args) {
+
+    }
+
+}
