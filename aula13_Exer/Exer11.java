0a806edae6ff7240201a690b3840221edf84361b
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-05-06 23:13:08 -0300

    Add files via upload

---
+++ aula13_Exer/Exer11.java
@@ -0,0 +1,15 @@
+public class Exer11 {
+
+    // Exercicio11 = Faça um programa que peça 2 numers inteiros e um numero real
+    // Calcule e mostre:
+    // a) o produto do dobro do primeiro com metade do sengundo
+    // b) a soma do triplo do primeiro com o terceiro
+    // c) o terceiro elevado ao cubo
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um número inteiro: ");
+        int 
+    }
+}
