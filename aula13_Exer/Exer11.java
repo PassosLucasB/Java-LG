63b729be5690777293a938cc125d5f7deefb4bd3
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-05-09 23:06:39 -0300

    Add files via upload

--- aula13_Exer/Exer11.java
+++ aula13_Exer/Exer11.java
@@ -1,4 +1,7 @@
+
 public class Exer11 {
+
+    import java.util.Scanner;
 
     // Exercicio11 = Faça um programa que peça 2 numers inteiros e um numero real
     // Calcule e mostre:
@@ -9,7 +12,16 @@ public class Exer11 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Digite um número inteiro: ");
-        int 
+        int num1 = scan.nextInt();
+
+        System.out.println("Digite um outro número inteiro: ");
+        int num2 = scan.nextInt();
+
+        System.out.println("Digite um número real: ");
+        double num3 = scan.nextDouble();
+
+        int resultado1 = (num1 * 2) *
     }
 }
