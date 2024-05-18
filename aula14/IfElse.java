8f62af2677f37bc259c5f2282527209f850eb57b
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-05-18 19:47:48 -0300

    Add files via upload

--- aula14/IfElse.java
+++ aula14/IfElse.java
@@ -4,12 +4,25 @@ public class IfElse {
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite a  idade");
+        /*System.out.println("Digite a  idade");
         int idade = scan.nextInt();
         if (idade >= 19){
             System.out.println("É maior de idade.");
         }else{
             System.out.println("Não é maior de idade.");
+        }*/
+
+        System.out.println("Digite o preço do item: ");
+        double valor = scan.nextDouble();
+
+        if (valor<= 10){
+            System.out.println("Está barato, pode comprar");
+        } else if (valor > 10 && valor <15){
+            System.out.println("Você pode pedir um desconto");
+        } else if (valor >= 15 && valor < 17){
+            System.out.println("Pode pesquisar mais");
+        } else { // valor >= 17
+            System.out.println("Muito caro");
         }
     }
 }
