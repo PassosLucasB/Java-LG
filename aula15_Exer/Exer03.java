108e986e61b5adccdf6c9dc9777555268bc68496
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-05-26 23:09:02 -0300

    Add files via upload

--- aula15_Exer/Exer03.java
+++ aula15_Exer/Exer03.java
@@ -1,2 +1,25 @@
+
+// Faça um programa que verifique se uma letra digitada é "F" ou "M".
+// Conforme a letra escrever : F - Feminino, M - Masculino  ou Sexo Inválido
+
+
+import java.util.Scanner;
+
 public class Exer03 {
+
+    public static void main (String [] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite uma letra (F ou M): ");
+        String input = scan.next();
+
+        if (input.equalsIgnoreCase("f")){
+            System.out.println("F - feminino");
+        } else if (input.equalsIgnoreCase("m")){
+            System.out.println("M - masculino");
+        } else {
+            System.out.println("Sexo inválido");
+        }
+    }
 }
