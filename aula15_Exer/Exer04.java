8cc0e5b754e56f3a969f21dfe26aca69c4ce17fe
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-06-04 21:50:04 -0300

    Add files via upload

--- aula15_Exer/Exer04.java
+++ aula15_Exer/Exer04.java
@@ -1,2 +1,25 @@
+
+// Faça um programa que verifique se uma letra digitada é vogal ou consoante
+
+import java.util.Scanner;
+
 public class Exer04 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite uma letra: ");
+        String letra = scan.next();
+
+        /*if (letra.equalsIgnoreCase("a") || letra.equalsIgnoreCase("e") ||
+                letra.equalsIgnoreCase("i") || letra.equalsIgnoreCase("o") ||
+                letra.equalsIgnoreCase("u")){
+            System.out.println("vogal");
+        } else {
+            System.out.println("consoante");
+        }*/
+
+        switch(letra)
+
+    }
 }
