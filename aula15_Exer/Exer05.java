e8341fd6682a5f9503ee8a33e323311e608990b2
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2024-07-01 23:27:11 -0300

    Add files via upload

--- aula15_Exer/Exer05.java
+++ aula15_Exer/Exer05.java
@@ -1,2 +1,36 @@
+
+/*
+Faça um programa para a leitura de duas notas parciais de um aluno.
+O programa deve carlcular a média alcançada por aluno e apresentar:
+ a) A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+ b) A mensagem "Reprovado", se a média for menor do que sete;
+ c) A mensagem "Aprovado com Distinção", se a média for igual a dez.
+ */
+
+import java.util.Scanner;
+
 public class Exer05 {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota:");
+        double nota1 = scan.nextDouble();
+        System.out.println("Digite a segunda nota:");
+        double nota2 = scan.nextDouble();
+
+        double media = (nota1 + nota2) /2;
+
+        if (media == 10){
+            System.out.println("Aprovado com distinção");
+        } else if (media >= 7){
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+
+    }
+
+
 }
